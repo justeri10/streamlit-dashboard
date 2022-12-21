3e4df7aea13e034c7ee7a7148cbdb7c73ca5b8cc
justeri10 <ustyuzhaninov01@gmail.com>
2022-12-21 19:40:37 -0300

    first commit

--- app.py
+++ app.py
@@ -743,13 +743,13 @@ df6 = df.loc[df['+date'].isin(['date on frame'])]
 
 st.text(f"Heatmap by date:")
 
-pivot = pd.pivot_table(df6, index='date', columns='hour', values='+date', aggfunc='count')
+pivot = pd.pivot_table(df6, index='date', columns='hour', values='ticket number', aggfunc='count')
 pivot.fillna(0, inplace=True)
 #pivot.sort_values(by='total', ascending=False, inplace=True)
 
-#piv =  px.imshow(pivot)
+piv =  px.imshow(pivot)
 
-#st.write(piv)
+st.write(piv)
 
 
 #HEATMAP
