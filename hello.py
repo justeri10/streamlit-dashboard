ed0af2cf7c5ee017a85ac6065d3e518df1366e6c
justeri10 <ustyuzhaninov01@gmail.com>
2022-12-27 14:59:11 -0300

    first commit

--- hello.py
+++ hello.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
 
+st.set_page_config(
+page_title = "Everyone Can Support",
+page_icon = ":phone:",
+#initial_sidebar_state="expanded",
+)
+
+
 st.write("# Welcome to support reports dashboard!")
