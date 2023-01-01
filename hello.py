7d1bf30df9b194e13cab722db37d33e894ec2958
justeri10 <ustyuzhaninov01@gmail.com>
2023-01-01 16:52:54 -0300

    first commit

--- hello.py
+++ hello.py
@@ -4,8 +4,9 @@ import streamlit as st
 st.set_page_config(
 page_title = "Everyone Can Support",
 page_icon = ":phone:",
-#initial_sidebar_state="expanded",
+initial_sidebar_state="expanded",
 )
 
 
 st.subheader("Welcome to support reports dashboard!")
+#st.caption("Developed by Ivan Ustyuzhaninov")
