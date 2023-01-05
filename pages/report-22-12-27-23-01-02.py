6b216f69fce47d75ecc47476d19c9a224500871f
justeri10 <ustyuzhaninov01@gmail.com>
2023-01-05 17:49:06 -0300

    first commit

--- pages/report-22-12-27-23-01-02.py
+++ pages/report-22-12-27-23-01-02.py
@@ -70,10 +70,27 @@ st.text("Report_status_2022-12-27_2023-01-02_1672746312.xlsx")
 
 # ---- SIDEBAR ----
 
+st.markdown("""---""")
+
+
+with st.expander("Efective solution >= 60 minutes (26 Tickets) NM Brazil"):
+    st.text("Efective_solution_2022-12-27_2023-01-02.xlsx")
+    st.dataframe(dfs) 
+
+with st.expander("Efective solution >= 60 minutes (26 Tickets) NM Latam"):
+    st.text("Efective_solution_2022-12-27_2023-01-02.xlsx")
+    
+
+with st.expander("Efective solution >= 60 minutes (26 Tickets) NM Indonesian"):
+    st.text("Efective_solution_2022-12-27_2023-01-02.xlsx")
+    
+    
+with st.expander("Efective solution >= 60 minutes (26 Tickets) NM English"):
+    st.text("Efective_solution_2022-12-27_2023-01-02.xlsx")    
 
 
 
-
+st.markdown("""---""")
 
 
 
@@ -266,15 +283,7 @@ with right_column:
     
     
 
-st.markdown("""---""")
 
-
-st.text("Efective solution >= 60 minutes (26 Tickets) NM Brazil")
-st.text("Efective_solution_2022-12-27_2023-01-02.xlsx")
-
-
-
-st.dataframe(dfs) 
 #st.dataframe(data=dfs, width=300, height=300, use_container_width=True)
 
 
